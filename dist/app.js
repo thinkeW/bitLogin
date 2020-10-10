cfaea8c51170988e38c1eda44ef3fd9dae8bb2bb
xinkewu <xinkewu@tencent.com>
2020-10-10 13:01:32 +0800

    变更配置方式

--- dist/app.js
+++ dist/app.js
@@ -6,21 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference path = "./app.d.ts" />
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
-const config_1 = __importDefault(require("./config"));
 const morgan_1 = __importDefault(require("morgan"));
 const url_1 = __importDefault(require("url"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = express_1.default();
 // 日志
 app.use(morgan_1.default('short'));
 // https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=http:/xxxx.com&response_type=code&scope=snsapi_base&state=2#wechat_redirect
 // 重定向到微信登陆
 app.get('/', (req, res) => {
-    let target = config_1.default.ID2url[req.query['state']];
+    let target = key2url(req.query['state']);
     if (!target) {
         res.json({
             code: 403,
             msg: '参数缺失or错误 或 目标地址未配置',
         });
+        return;
     }
     let myhost = url_1.default.format({
         protocol: req.protocol,
@@ -28,23 +30,24 @@ app.get('/', (req, res) => {
     });
     myhost += '/code2openid';
     // 重定向
-    res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${config_1.default.appid}&redirect_uri=${encodeURIComponent(myhost)}&response_type=code&scope=snsapi_base&state=${req.query['state']}#wechat_redirect`);
+    res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${process.env.APPID}&redirect_uri=${encodeURIComponent(myhost)}&response_type=code&scope=snsapi_base&state=${req.query['state']}#wechat_redirect`);
 });
 app.get('/code2openid', (req, res) => {
     // 获取到url上的code
     const code = req.query['code'];
-    let target = config_1.default.ID2url[req.query['state']];
+    let target = key2url(req.query['state']);
     console.log({ code, target });
     if (!(code && target)) {
         res.json({
             code: 403,
             msg: '参数缺失or错误 或 目标地址未配置',
         });
+        return;
     }
     target = encodeURIComponent(target);
     //
     axios_1.default
-        .get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config_1.default.appid}&secret=${config_1.default.secret}&code=${code}&grant_type=authorization_code`)
+        .get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${process.env.APPID}&secret=${process.env.SECRET}&code=${code}&grant_type=authorization_code`)
         .then((RV) => {
         const data = RV.data;
         if (!data.openid) {
@@ -62,7 +65,7 @@ app.get('/code2openid', (req, res) => {
         });
     });
 });
-console.log(app.listen(config_1.default.port).address());
+console.log(app.listen(process.env.PORT).address());
 /* 开启 https
  *
  *
@@ -70,3 +73,6 @@ http.createServer(app).listen(80);
 https.createServer({  }, app).listen(443);
  *
  */
+function key2url(key) {
+    return process.env[key] || '';
+}
